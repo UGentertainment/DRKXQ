9084e05ef0ae770fef068cf4e83c0291718db4af
UGentertainment <UGentertainment@users.noreply.github.com>
2026-09-09 12:13:14 +0800

    Use canvas renderer for tainted dialogue textures

---
+++ tools/test-renderer-config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const vm = require('vm');
+
+const sandbox = {};
+vm.createContext(sandbox);
+vm.runInContext(
+    fs.readFileSync(path.join(__dirname, '..', 'js', 'plugins.js'), 'utf8'),
+    sandbox
+);
+
+const communityBasic = sandbox.$plugins.find(function(plugin) {
+    return plugin.name === 'Community_Basic';
+});
+
+assert(communityBasic, 'Community_Basic plugin is missing');
+assert.strictEqual(communityBasic.parameters.renderingMode, 'canvas');
+console.log('renderer configuration test passed');
